a711d83eb3b1bfd1a99c69a90f9c6bfd068b997b
aSujal <Sujalgandhi.9999@gmail.com>
2023-06-29 09:06:26 +0200

    Added all the neccessary folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { RecoilRoot } from "recoil";
 import logo from "./logo.svg";
 import "./App.css";
 import Cell from "./compenents/Cell/Cell";
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <Cell cellid="A1">A1</Cell>
       <Cell cellid="B2">B2</Cell>
-    </>
+    </RecoilRoot>
   );
 }
 
--- src/compenents/Cell/Cell.tsx
+++ src/compenents/Cell/Cell.tsx
@@ -1,22 +1,31 @@
 import React, {
+  ChangeEvent,
   ComponentType,
   FunctionComponent,
   useEffect,
   useRef,
   useState,
 } from "react";
+import { atom, useRecoilState } from "recoil";
 import classes from "./Cell.module.css";
+import { CellValueState } from "../../store/CellValueState";
+
 export type CellProps = {
   children: string;
   cellid: string;
 };
 const Cell: FunctionComponent<CellProps> = (props) => {
+  const [cellValue, setCellValue] = useRecoilState<string>(CellValueState);
+
   const [isEditMode, setIsEditMode] = useState(false);
-  const inputRef = useRef(null); //to set the reference using ref={inputRef} to this input object
+  const inputRef = useRef(null); //to set the reference using ref={inputRef} to this input object this allows for direct access to dom properties
 
   //functions to change from input to label and label to input
   const changeLabelToInput = () => setIsEditMode(true);
   const changeInputToLabel = () => setIsEditMode(false);
+
+  const updateCellValueState = (event: ChangeEvent<HTMLInputElement>) =>
+    setCellValue(event.target.value);
 
   /*It checks if the clicked element's 
     dataset.cellId is different from the current cellid prop value. If they are different,
@@ -34,10 +43,15 @@ const Cell: FunctionComponent<CellProps> = (props) => {
   });
 
   return isEditMode ? (
-    <input ref={inputRef} data-cell-id={props.cellid} />
+    <input
+      ref={inputRef}
+      data-cell-id={props.cellid}
+      value={cellValue}
+      onChange={updateCellValueState}
+    />
   ) : (
     <div data-cell-id={props.cellid} onClick={changeLabelToInput}>
-      {props.children}
+      {cellValue} {/* value */}
     </div>
   );
 };
