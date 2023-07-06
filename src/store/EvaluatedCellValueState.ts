7d32516abd02f066f1318ee2226cc1914e1eab88
aSujal <Sujalgandhi.9999@gmail.com>
2023-07-06 09:44:45 +0200

    removed unnecessary imports

--- src/store/EvaluatedCellValueState.ts
+++ src/store/EvaluatedCellValueState.ts
@@ -1,27 +1,24 @@
-import {RecoilValueReadOnly, selector } from "recoil";
+import { selector } from "recoil";
 import { CellValueState } from "./CellValueState";
 import { memoize } from "../utils/memoize";
-import { evaluate } from "mathjs";
 import { evaluateEquation } from "../utils/EquationUtils";
 
-export const EvaluatedCellValueState = (cellId: string) => 
-    memoize(`evaluatedCell_${cellId}`, () =>
+export const EvaluatedCellValueState = (cellId: string) =>
+  memoize(`evaluatedCell_${cellId}`, () =>
     selector({
-        key: `evaluatedCell_${cellId}`,
-        get: ({ get }) => {
-            const value = get(CellValueState(cellId)) as string;
-            if(value.startsWith('=')){
-                try {
-                    const equation = value.substring(1);
-                    const evaluatedExpression = evaluateEquation(get, equation);
-                    return evaluatedExpression;
-                  }
-                catch {
-                    return value;
-                }
-            }
+      key: `evaluatedCell_${cellId}`,
+      get: ({ get }) => {
+        const value = get(CellValueState(cellId)) as string;
+        if (value.startsWith("=")) {
+          try {
+            const equation = value.substring(1);
+            const evaluatedExpression = evaluateEquation(get, equation);
+            return evaluatedExpression;
+          } catch {
             return value;
-        },
+          }
+        }
+        return value;
+      },
     })
-);
-
+  );
